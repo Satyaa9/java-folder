1374940f56fdbfae3f1c1db908fa6ad786dfbbfb
Satish <dadassatish6@gmail.com>
2025-05-10 22:44:21 +0530

    new java file

---
+++ p12.java
@@ -0,0 +1,26 @@
+import java.lang.System;
+import java.lang.String;
+
+class demo12{
+	public static void main(String[]args){
+	System.out.println("Hello world");
+	codex.fun();
+	codex.gun();
+	}
+}
+
+class codex{
+	static void fun(){
+	System.out.println("is fun");
+	}
+	static void gun(){
+	System.out.println("not a gun");
+	}
+}
+
+/*OUTPUT: 	Hello world
+		is fun
+		not a gun*/
+
+
+
---
+++ p2.java
@@ -0,0 +1,11 @@
+class demo{
+	static void main(String[]args){
+	System.out.println("Hello world");
+  		}
+
+
+	}
+
+
+/*Error: Main method not found in class demo, please define the main method as:
+   public static void main(String[] args)*/
