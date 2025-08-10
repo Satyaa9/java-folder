a41aba7a996260c0bdb6bc2278b9c7e2ae5241c4
Satish <dadassatish6@gmail.com>
2025-08-10 22:47:08 +0530

    new added

---
+++ passing primary data type to method/p5.java
@@ -0,0 +1,50 @@
+class Company{
+	String companyName;
+	int employeNos;
+
+	Company(String companyName,int employeNos){
+		this.companyName=companyName;
+		this.employeNos;
+	}
+	void setcompanyName(String companyName){
+		this.companyName;
+	}
+	void setemployeNos(int employeNos){
+		this.employeNos;
+	}
+	
+	String getcompany(){
+		return company;
+	}
+	int getemployeNos(){
+		return employeNos;
+	}
+}
+class employee{
+	String name;
+
+	employee(String name){
+		this.name;
+	}
+	void setName(String name){
+		this.name;
+	}
+	String getName(){
+		return name;
+	}
+}
+class Demo{
+	public static void main(String[]args){
+	
+	Company coditas = new Company("Coditas",500);
+	System.out.println("company name is"+setscompanyName);
+		
+	}
+}
+
+
+
+
+
+
+
