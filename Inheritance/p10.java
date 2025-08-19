19234fe593446ece7429bc52652c90d95fc93521
Satish <dadassatish6@gmail.com>
2025-08-19 07:08:54 +0530

    new added

---
+++ Inheritance/p10.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String[]args){
+	
+		String name = "Shubham";
+		String surName = "Mane";
+		
+		System.out.println(System.identityHashCode(name));
+		System.out.println(System.identityHashCode(surName));
+		System.out.println(name);
+		System.out.println(surName);
+		
+	}
+}
---
+++ string in java/p16.java
@@ -0,0 +1,8 @@
+class Demo{
+	public static void main(String args[]){
+		int num=10;
+		char num;
+		System.out.println(num);
+	}
+}
+	
