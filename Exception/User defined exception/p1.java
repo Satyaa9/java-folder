40a30fd9a7deb52d15290f4e307b0f7637cbd850
Satish <dadassatish6@gmail.com>
2025-09-10 19:48:41 +0530

    new added

---
+++ Exception/User defined exception/p1.java
@@ -0,0 +1,14 @@
+class MyException extends Exception{
+
+}
+class Demo{
+	public static void main(String[]args){
+		throw new MyException();
+	}
+}
+
+/*
+OUTPUT:
+ error: unreported exception MyException; must be caught or declared to be thrown
+                throw new MyException();
+*/
