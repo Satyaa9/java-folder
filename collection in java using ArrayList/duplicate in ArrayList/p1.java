5551980e2ea109e0f46771a33f3ed1d842ed34ab
Satish <dadassatish6@gmail.com>
2025-09-17 17:01:38 +0530

    new added

---
+++ collection in java using ArrayList/duplicate in ArrayList/p1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Demo{
+	public static void main(String[]args){
+		
+		ArrayList al = new ArrayList();
+
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(30);
+
+		System.out.println(al);
+	}
+}
+
+/*
+OUTPUT:
+[10, 20, 30, 30]
+
+ArrayList madhe duplicate element chaltat.
+*/
---
+++ wrapper class/p1.java
@@ -0,0 +1,17 @@
+class Demo{
+	public static void main(String[]args){
+		int a=10;
+		Integer b =new Integer(20);
+
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+/*
+OUTPUT:
+10
+20
+
+Karan integer ha aik java madhe class lihun thevla aaplya help sathi karan aapan tya class madhlya method use karavya mhanun
+*/
