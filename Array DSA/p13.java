29b2b216aa9e3035b61c6ff8b8406c36cb970234
Satish <dadassatish6@gmail.com>
2025-10-09 23:53:34 +0530

    new

--- Array DSA/p13.java
+++ Array DSA/p13.java
@@ -1,10 +1,25 @@
 import java.util.*;
 class Demo{
 	public static void main(String[]args){
-		int [] arr={1,2,3,4,5};
 
-		int max=0;
-		int second_max=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a array size :");
+		int n=sc.nextInt();
+
+		int [] arr=new int[n];
+		System.out.print("Enter a array elements :");
+
+		int max=Integer.MIN_VALUE;
+		int smax=Integer.MIN_VALUE;
+
+		int min=Integer.MAX_VALUE;
+		int smin=Integer.MAX_VALUE;
+
+
+		for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+		}
+
 		for(int i=0;i<arr.length;i++){
 			if(arr[i]>max){
 				max=arr[i];
@@ -12,11 +27,34 @@ class Demo{
 		}
 
 		for(int i=0;i<arr.length;i++){
-			if(arr[i]>second_max && second_max!=max){
-				second_max=arr[i];
+			if(arr[i]>smax && arr[i]!=max){
+				smax=arr[i];
 			}
 		}
-		System.out.print(max + " ");
-		System.out.print(second_max+ " ");
+
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]<min){
+				min=arr[i];
+			}
+		}
+
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]<smin && arr[i]!=min){
+				smin=arr[i];
+			}
+		}
+		System.out.println("------------------------------------");
+		System.out.print("Second biggest number in array :"+smax+ " ");
+		System.out.println();
+		System.out.print("Second smalles number in array :"+smin+" ");
 	}
 }
+
+/*
+OUTPUT:
+Enter a array size :5
+Enter a array elements :13 4 5 10 44
+------------------------------------
+Second biggest number in array :13
+Second smalles number in array :5
+*/
