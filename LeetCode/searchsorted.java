489fe923a84c753eb48eab00d333221d82475c6e
Satish <dadassatish6@gmail.com>
2025-12-04 11:51:46 +0530

    new

--- LeetCode/searchsorted.java
+++ LeetCode/searchsorted.java
@@ -1,5 +1,42 @@
-class Demo{
-	public int search(int [] a , int target){
-		int l = 0 , h = a.length - 1 , 
+class Demo {
+	public int search(int[] a, int target) {
+		int l = 0, h = a.length - 1, m = 0;
+
+		while (l <= h) {
+			m = (l + h) / 2;
+
+			if (target == a[m]) {
+				return m;
+			} else if (a[l] <= a[m]) {
+				if (target >= a[l] && target < a[m]) {
+					h = m - 1;
+				} 
+				else {
+					l = m + 1;
+				}
+			}
+		 else {
+			if(target > a[m] && target <= a[h]){
+				l = m + 1;
+			}
+			else{
+				h = m-1;
+			}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Demo demo = new Demo();
+		int[] a = { 4, 5, 6, 7, 8, 1, 2 };
+
+		int result = demo.search(a, 7);
+		System.out.println("Index :" + result);
 	}
 }
+
+/*
+OUTPUT :
+Index :3
+*/
