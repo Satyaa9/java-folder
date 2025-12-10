1b5d0834954724b6ab8e80fe6fc2b69342c94bdc
Satish <dadassatish6@gmail.com>
2025-12-10 17:35:42 +0530

    new

---
+++ operators java/DSA/p1.java
@@ -0,0 +1,24 @@
+class Demo{
+	public static void main(String [] args){
+	
+	int num = 123;
+
+	int a = num/100;
+	int b = (num/10)%10;
+	int c = (num%100)%10;
+
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	}
+}
+
+/*
+
+OUTPUT:
+
+1
+2
+3
+
+*/
