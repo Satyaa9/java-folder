921e74e94983ea25d33a232bb3153564275d82b0
Satish <dadassatish6@gmail.com>
2025-12-11 19:56:46 +0530

    new

---
+++ Array DSA/p39.java
@@ -0,0 +1,17 @@
+class Demo{
+	public static void main(String [] args){
+		int sqa = 1;
+		int num = 10;
+
+		for(int i = 1 ; i <=num ; i++){
+			sqa = i*i;
+			System.out.print(sqa+" ");
+		}
+		
+	}
+}
+
+/*
+OUTPUT:
+1 4 9 16 25 36 49 64 81 100
+*/
