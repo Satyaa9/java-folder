ee58cd8fa3f90ff8e15717a1d73215b5777df9af
Satish <dadassatish6@gmail.com>
2025-12-18 20:08:38 +0530

    new

---
+++ array in java/p48.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Demo{
+	public static void main(String[] args){
+		int [] arr = {5,2,8,1};
+
+		//Arrays.sort(arr);
+
+		for(int i = 0 ; i < arr.length ; i++){
+			for(int j = i + 1 ; j < arr.length ; j++){
+				if(arr[i] > arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
+
+/*
+OUTPUT:
+1 2 5 8
+*/
